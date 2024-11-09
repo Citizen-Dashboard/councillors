be56883ab21d7b10f63340f2e90ead98242c9170
Matt Orchard <morch028@uottawa.ca>
2024-11-08 21:04:07 -0500

    Adds app navigation to pages

---
+++ src/components/app-navigation.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useIsMobile } from "@/hooks/use-mobile";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerTitle,
+  DrawerHeader,
+  DrawerTrigger,
+  DrawerDescription,
+} from "@/components/ui/drawer";
+import Link from "next/link";
+import { FC } from "react";
+import { MenuIcon } from "lucide-react";
+
+export const AppNavigation = () => {
+  const isMobile = useIsMobile();
+
+  return (
+    <header className="flex items-center py-1 px-2 gap-2 sticky top-0 bg-white border-b-2 border-slate-200">
+      {isMobile && <NavigationHamburger />}
+      <Link href="/">
+        <h1 className="text-2xl font-semibold">Citizen Dashboard</h1>
+      </Link>
+      {!isMobile && <NavigationContent orientation="horizontal" />}
+    </header>
+  );
+};
+
+const NavigationHamburger = () => {
+  return (
+    <Drawer>
+      <DrawerTrigger>
+        <MenuIcon aria-label="Navigation menu" />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="sr-only">
+          <DrawerTitle>Navigation</DrawerTitle>
+          <DrawerDescription>Site navivation menu</DrawerDescription>
+        </DrawerHeader>
+        <NavigationContent orientation="vertical" />
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+const NavigationContent: FC<{
+  orientation: "vertical" | "horizontal";
+}> = ({ orientation }) => (
+  <NavigationMenu orientation={orientation} className="w-full">
+    <NavigationMenuList orientation={orientation}>
+      <NavMenuLink text="Councillors" href="/councillors" />
+      <NavMenuLink text="Wards" href="/wards" />
+      <NavMenuLink text="Committees" href="/committees" />
+      <NavMenuLink text="How council works" href="/how-council-works" />
+      <NavMenuLink text="About us" href="/about-us" />
+    </NavigationMenuList>
+  </NavigationMenu>
+);
+
+const NavMenuLink: FC<{ text: string; href: string }> = ({ text, href }) => (
+  <NavigationMenuItem>
+    <Link href={href} legacyBehavior passHref>
+      <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+        {text}
+      </NavigationMenuLink>
+    </Link>
+  </NavigationMenuItem>
+);
